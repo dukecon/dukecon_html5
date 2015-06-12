2032d01d843cb2a5e010e1d211cafb617cb44a5c
annam002 <annah002@gmail.com>
2015-06-12 13:44:44 +0200

    Add day filter

--- js/dukecon.js
+++ js/dukecon.js
@@ -19,8 +19,13 @@ var utils = {
         }
     },
 
+    //format is: 2015-03-25T09:00
     getDisplayDate : function(datetimeString) {
-        return this.splitDate(datetimeString, 'T')[0];
+        var dateString = this.splitDate(datetimeString, 'T')[0];
+        var parsed = dateString.split("-");
+        var month = parsed[1];
+        var day = parsed[2];
+        return day + "." + month;
     },
 
     getDisplayTime : function(datetimeString) {
@@ -31,7 +36,8 @@ var utils = {
 
 function Talk(data) {
     this.id = data.id;
-    this.startDisplayed = utils.getDisplayDate(data.start) + ", " + utils.getDisplayTime(data.start);
+    this.day = utils.getDisplayDate(data.start);
+    this.startDisplayed = this.day + ", " + utils.getDisplayTime(data.start);
     this.startSortable = data.start;
     this.track = data.track;
     this.location = data.location;
@@ -69,7 +75,6 @@ function TalkListViewModel() {
     self.talks = ko.observableArray([]);
     self.allTalks = [];
 
-    // Sorting by column
     self.headers = [
         { title:'Time', sortKey:'startSortable', asc: true, cssClass: 'clickable' },
         { title:'Track', sortKey:'track', asc: true, cssClass: 'clickable' },
@@ -78,7 +83,81 @@ function TalkListViewModel() {
         { title:'Abstract', sortKey:'', asc: true, cssClass: '' }
     ];
 
-    self.activeSort = self.headers[0]; //set the default sort
+    self.activeSort = self.headers[0]; //default sort
+
+    self.filters = [
+        //{title: 'Day', filterKey: 'start', values: []},
+        {title: 'Level', filterKey: 'level', filtervalues : ko.observableArray([]), selected : ko.observableArray([])},
+        {title: 'Language', filterKey: 'language', filtervalues : ko.observableArray([]), selected : ko.observableArray([])},
+        {title: 'Track', filterKey: 'track', filtervalues : ko.observableArray([]), selected : ko.observableArray([])},
+        //{title: 'Speaker', filterKey: 'speakers', values: []},
+        {title: 'Room', filterKey: 'location', filtervalues : ko.observableArray([]), selected : ko.observableArray([])}
+    ];
+
+    self.days = ko.observableArray([]);
+    self.selectedDay = "";
+
+    // Initialize
+    $.each(self.filters, function(index, filter) {
+        filter.selected.subscribe(function(s) {
+            self.filterTalks();
+        });
+    });
+
+    $.ajax({
+      method: 'GET',
+      dataType: "json",
+      url: jsonUrl,
+      success: function(allData) {
+         self.initializeData(allData);
+      },
+      error: function(error) {
+        console.log("Nothing updated. Device offline?");
+      }
+    });
+
+    self.initializeData = function(allData) {
+        var mappedTalks = $.map(allData, function(item) { return new Talk(item) });
+        self.talks(mappedTalks);
+        self.allTalks = mappedTalks;
+        self.days(self.getDistinctValues('day'));
+        self.selectedDay = self.days()[0];
+        self.addFilters();
+        self.filterTalks();
+        console.log("Talks updated - " + utils.getFormattedDate(new Date()));
+    };
+
+    // Functions
+    self.addFilters = function() {
+        $.each(self.filters, function(index, filter) {
+            filter.filtervalues(self.getDistinctValues(filter.filterKey));
+        });
+    };
+
+    self.getDistinctValues = function(key) {
+        var t = _.groupBy(self.allTalks, function(talk) {
+            return talk[key];
+        });
+        return _.keys(t).sort();
+    };
+
+    self.filterTalks = function() {
+        self.talks(_.filter(self.allTalks, function(talk) {
+            return talk.day === self.selectedDay && _.every(self.filters, function(filter) {
+                if (filter.selected().length === 0) {
+                    return true;
+                }
+                return _.some(filter.selected(), function(selected) {
+                    return talk[filter.filterKey] === selected;
+                })
+            });
+        }));
+    };
+
+    self.updateDay = function(day) {
+        self.selectedDay = day;
+        self.filterTalks();
+    }
 
     self.sort = function(header, event){
         //if this header was just clicked a second time
@@ -93,66 +172,6 @@ function TalkListViewModel() {
         var sortFunc = self.activeSort.asc ? ascSort : descSort;
         self.talks.sort(sortFunc);
     };
-
-    // filter
-    self.addFilters = function() {
-        $.each(self.filters, function(index, filter) {
-          filter.filtervalues(self.getDistinctValues(filter.filterKey));
-        });
-    };
-
-    self.getDistinctValues = function(key) {
-        var t = _.groupBy(self.allTalks, function(talk) {
-            return talk[key];
-        });
-        return _.keys(t).sort();
-    };
-
-    self.filters = [
-        //{title: 'Day', filterKey: 'start', values: []},
-        {title: 'Level', filterKey: 'level', filtervalues : ko.observableArray([]), selected : ko.observableArray([])},
-        {title: 'Language', filterKey: 'language', filtervalues : ko.observableArray([]), selected : ko.observableArray([])},
-        {title: 'Track', filterKey: 'track', filtervalues : ko.observableArray([]), selected : ko.observableArray([])},
-        //{title: 'Speaker', filterKey: 'speakers', values: []},
-        {title: 'Room', filterKey: 'location', filtervalues : ko.observableArray([]), selected : ko.observableArray([])}
-    ];
-
-    $.each(self.filters, function(index, filter) {
-        filter.selected.subscribe(function(s) {
-            self.filterTalks();
-        });
-    });
-
-    self.filterTalks = function() {
-        self.talks(_.filter(self.allTalks, function(talk) {
-           return _.every(self.filters, function(filter) {
-               if (filter.selected().length === 0) {
-                   return true;
-               }
-               return _.some(filter.selected(), function(selected) {
-                   return talk[filter.filterKey] === selected;
-                })
-           });
-        }));
-    };
-
-    // Get data
-    $.ajax({
-      method: 'GET',
-      dataType: "json",
-      url: jsonUrl,
-      success: function(allData) {
-          var mappedTalks = $.map(allData, function(item) { return new Talk(item) });
-          self.talks(mappedTalks);
-          self.allTalks = mappedTalks;
-          self.addFilters();
-          console.log("Talks updated - " + utils.getFormattedDate(new Date()));
-      },
-      error: function(error) {
-        console.log("Nothing updated. Device offline?");
-      }
-    });
-
 }
 
 ko.applyBindings(new TalkListViewModel());
