58ecaeda5eda4146fbfd60ab29608069467e8078
Gerd Aschemann <gerd@aschemann.net>
2015-09-11 16:42:51 +0200

    Added Cache reload (not yet working!)

--- src/main/webapp/public/js/offline.js
+++ src/main/webapp/public/js/offline.js
@@ -1,3 +1,20 @@
+// Check if a new cache is available on page load.
+window.addEventListener('load', function(e) {
+    console.log ("Adding event listener for cache updates");
+    window.applicationCache.addEventListener('updateready', function(e) {
+        if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+            // Browser downloaded a new app cache.
+            // Swap it in and reload the page to get the new hotness.
+            window.applicationCache.swapCache();
+            if (confirm('A new version of this site is available. Load it?')) {
+                window.location.reload();
+            }
+        } else {
+            // Manifest didn't changed. Nothing new to server.
+        }
+    }, false);
+}, false);
+
 var dukeconTalkUtils = {
     getData : function(callback) {
         var successCallback = function(data) {
