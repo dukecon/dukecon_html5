0cc1efb4817ac6b704944eda31bf6b857501954d
annam002 <annah002@gmail.com>
2015-09-20 19:57:28 +0200

    Replace popup with log message

--- src/main/webapp/public/js/offline.js
+++ src/main/webapp/public/js/offline.js
@@ -2,15 +2,13 @@
 window.addEventListener('load', function(e) {
     window.applicationCache.addEventListener('updateready', function(e) {
         if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+            console.log("Loading new version of page");
             // Browser downloaded a new app cache.
             // Swap it in and reload the page to get the new hotness.
             window.applicationCache.swapCache();
-            if (confirm('A new version of this site is available. Load it?')) {
-                window.location.reload();
-            } else {
-            	console.log ("The reload of the site was refused by the user");
-            }
-        } else {
+            window.location.reload();
+        }
+        else {
             console.log ("The manifest did not change!");
         }
     }, false);
