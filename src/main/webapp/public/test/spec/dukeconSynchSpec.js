8a606f06ac29eee7f7d7bffed5a157418d9379cf
annam002 <annah002@gmail.com>
2016-01-08 16:32:39 +0100

    #30: Add Jasmine tests for pull; clean up after test

--- src/main/webapp/public/test/spec/dukeconSynchSpec.js
+++ src/main/webapp/public/test/spec/dukeconSynchSpec.js
@@ -1,25 +1,47 @@
-describe("dukecon - Synch", function() {
+describe("dukeconSynch", function() {
+
+    var realDukeCloak;
+    var realTalklistModel;
 
     beforeEach(function() {
         jasmine.Ajax.install();
-        //fake dukecloak
+        if (window.dukecloak) {
+            realDukeCloak = window.dukecloak;
+        }
         window.dukecloak = {
             keycloakAuth :
-                { token  : '',
-                    authenticated : true,
-                    updateToken : function() {
-                        return {
-                            success : function(callback) {
-                                callback();
-                                return this;
-                            },
-                            error : function() { /* Nothing to do */ }
-                        }
+            { token  : '',
+                authenticated : true,
+                updateToken : function() {
+                    return {
+                        success : function(callback) {
+                            callback();
+                            return this;
+                        },
+                        error : function() { /* Nothing to do */ }
                     }
-                }};
+                }
+            }};
+        if (window.dukeconTalklistModel) {
+            realTalklistModel = window.dukeconTalklistModel;
+        }
+        window.dukeconTalklistModel = {
+            updateFavourites : function() { /* Nothing to do */ }
+        };
     });
 
-    it("synch favourites", function() {
+    afterEach(function() {
+        jasmine.Ajax.uninstall();
+        if (realDukeCloak) {
+            window.dukecloak = realDukeCloak;
+        }
+        if (realTalklistModel) {
+            window.dukeconTalklistModel = realTalklistModel;
+        }
+    });
+
+    it("push", function() {
+
         spyOn(dukeconSettings, 'getFavourites').and.returnValue(['509516', '509557']);
 
         dukeconSynch.push();
@@ -29,4 +51,55 @@ describe("dukecon - Synch", function() {
         expect(request.method).toBe('POST');
         expect(request.data()).toEqual([{ eventId : '509516' , version : '1' },{ eventId : '509557' , version : '1'}]);
     });
+
+    it("pull - merge", function() {
+
+        spyOn(dukeconTalklistModel, "updateFavourites");
+        spyOn(dukeconSynch, "setToken").and.returnValue(null);
+        spyOn(dukeconSynch, "push").and.returnValue(null);
+        spyOn(dukeconSettings, "getFavourites").and.returnValue(['3', '4']);
+
+        spyOn(dukeconSettings, "saveSetting").and.returnValue(null);
+
+        dukeconSynch.pull();
+
+        var request = jasmine.Ajax.requests.mostRecent();
+        expect(request.url).toBe('rest/preferences');
+        expect(request.method).toBe('GET');
+
+        request.respondWith({
+            status: 200,
+            responseText: '[{"eventId":"1","version":0},{"eventId":"2","version":0},{"eventId":"3","version":0}]'
+        });
+
+        expect(dukeconSettings.saveSetting).toHaveBeenCalledWith("dukeconfavs", ["1", "2", "3", "4"]);
+        expect(dukeconSynch.push).toHaveBeenCalled();
+        expect(dukeconTalklistModel.updateFavourites).toHaveBeenCalled();
+    });
+
+    it("pull - no changes", function() {
+        spyOn(dukeconTalklistModel, "updateFavourites");
+        spyOn(dukeconSynch, "setToken").and.returnValue(null);
+        spyOn(dukeconSynch, "push").and.returnValue(null);
+        spyOn(dukeconSettings, "getFavourites").and.returnValue(['3', '4']);
+
+        spyOn(dukeconSettings, "saveSetting").and.returnValue(null);
+
+        dukeconSynch.pull();
+
+        var request = jasmine.Ajax.requests.mostRecent();
+        expect(request.url).toBe('rest/preferences');
+        expect(request.method).toBe('GET');
+
+        request.respondWith({
+            status: 200,
+            responseText: '[{"eventId":"3","version":0},{"eventId":"4","version":0}]'
+        });
+
+        expect(dukeconSettings.saveSetting.calls.any()).toEqual(false);
+        expect(dukeconSynch.push.calls.any()).toEqual(false);
+        expect(dukeconTalklistModel.updateFavourites.calls.any()).toEqual(false);
+    });
 });
+
+
