481692f8832a8003c6482207746476106e823b5f
Alexander Schwartz <alexander.schwartz@gmx.net>
2016-01-09 15:20:54 +0100

    initialize dukecloak only on pages where it is available

--- src/main/webapp/public/js/offline.js
+++ src/main/webapp/public/js/offline.js
@@ -39,7 +39,9 @@ function setOfflineStatus(offline) {
         setInterval(function() {
             dukeconTalkUtils.checkNewDataOnServer();
         }, 300000);
-        dukecloak.init();
+        if(typeof dukecloak !== 'undefined') {
+            dukecloak.init();
+        }
     }
     return doPageReload;
 }
