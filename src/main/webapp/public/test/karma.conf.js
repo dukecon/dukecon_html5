355b27a289977ec9931af08f666c5c4357037d81
annam002 <annah002@gmail.com>
2016-01-17 22:50:24 +0100

    #8: Add karma config for automatic running of tests and coverage report

---
+++ src/main/webapp/public/test/karma.conf.js
@@ -0,0 +1,26 @@
+module.exports = function(config) {
+    config.set({
+        basePath: '.',
+        dir: 'coverage/',
+        frameworks: ['jasmine'],
+        plugins : ['karma-coverage', 'karma-jasmine', 'karma-chrome-launcher'],
+        files: [
+            'jasmine/jasmine.js',
+            'jasmine/jasmine-html.js',
+            'jasmine/boot.js',
+            'jasmine/mock-ajax.js',
+            '../js/jquery-2.1.4.min.js',
+            '../js/knockout-min-3.3.0.js',
+            '../js/underscore-min.js',
+            '../js/dukecon.js',
+            '../js/offline.js',
+            '../js/talklist.js',
+            '../js/ui.js',
+            'spec/*.js'
+        ],
+        browsers: ['Chrome'],
+        singleRun: true,
+        reporters: ['progress', 'coverage'],
+        preprocessors: { '../js/*.js': ['coverage'] }
+    });
+};
