d5e883fc9bb9424c38e9613f93f7b6be070169ef
Gerd Aschemann <gerd@aschemann.net>
2016-02-19 01:01:36 +0100

    Applied solution for #57 to R 1.1

--- src/main/webapp/public/js/dukecon.js
+++ src/main/webapp/public/js/dukecon.js
@@ -26,7 +26,7 @@ function Talk(data, speakers, metaData, isFavourite) {
     this.speakersWithCompanies = dukeconUtils.getSpeakerNames(data.speakerIds, speakers, true);
     this.languageDisplay = ko.observable(dukeconUtils.getLanguage(metaData, data.languageId));
     this.language = data.languageId || '';
-    this.fullAbstract = data.abstractText || '';
+    this.fullAbstract = (data.abstractText || '').replace(/\n/g, "<br />");
     this.timeCategory =  dukeconDateUtils.getTimeCategory(this.duration);
     this.timeClass = this.timeCategory == 'regular' ? 'time' : 'time-extra';
     this.favourite = ko.observable(isFavourite);
