3b7c495d6b3305852a6944a6bf5641fc69180cba
stephaniep <s.peters@topdesk.com>
2017-03-03 16:38:44 +0100

    #84 alternate approach, collapsible filters (still todo: badge showing if anything is selected)

--- src/main/webapp/public/js/modules/talklist.js
+++ src/main/webapp/public/js/modules/talklist.js
@@ -65,6 +65,17 @@ define(['underscore', 'jquery', 'knockout', 'js/modules/dukeconsettings', 'js/mo
 
             self.onlyFavourites = ko.observable(dukeconSettings.favoritesActive());
             self.filtersActive = ko.observable(dukeconSettings.filtersActive());
+            
+            self.expandCollapseFilter = function(unused, event) {
+                var collapsedCss = "collapsed";
+                var targetElem = event.currentTarget.parentNode.parentNode; // the one with class "collapsed"
+                if (targetElem.className === collapsedCss) {
+					targetElem.className = "";
+                }
+                else {
+					targetElem.className = collapsedCss;
+                }
+            };
 
             //temporarily pause filter update actions
             self.updateFiltersPaused = false;
