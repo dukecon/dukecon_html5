986f1851756555edef4c1c768405f1c197a380d9
Gerd Aschemann <gerd@aschemann.net>
2017-03-18 00:43:50 +0100

    Fix domReady indentation

--- src/main/webapp/public/app-talkschedule.js
+++ src/main/webapp/public/app-talkschedule.js
@@ -2,7 +2,7 @@ require.config({
     paths: {
         "jquery": "js/jquery-2.1.4.min",
         "underscore": "js/underscore-min",
-		"domReady": "js/domReady",
+	"domReady": "js/domReady",
         "knockout": "js/knockout-min-3.3.0",
         "moment": "js/moment-with-locales-2.11.2",
         "vis": "js/vis"
